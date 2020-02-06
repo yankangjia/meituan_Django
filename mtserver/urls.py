123af4278ea7a035816982783e5ccea78b72905e
ykj <943170356@qq.com>
2020-02-06 17:37:40 +0800

    modify urls index

--- mtserver/urls.py
+++ mtserver/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    re_path('^$',TemplateView.as_view(template_name='index.html'),name='index'),
     path('',include('apps.h5.urls')),
     path('cms/',include('apps.cms.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
